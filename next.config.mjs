f22f2bb2e735c7b77d9611082cc5891b7096d5de
trungdt <trungdoan7373@gmail.com>
2024-02-29 11:38:41 +0700

    update nextjs.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
-        port: "",
       },
     ],
   },
